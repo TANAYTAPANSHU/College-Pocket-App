8c9d76cd815f5f60340855b02cbb0dd2bfc5fc63
TANAYTAPANSHU <42114467+TANAYTAPANSHU@users.noreply.github.com>
2021-06-22 14:17:27 +0530

    Firebase setup

---
+++ CollegeBeats/config.js
@@ -0,0 +1,17 @@
+export const  firebaseConfig = {
+        apiKey: "AIzaSyDxTKx66EJUweyv9Sp_7suIyzJDhkBC5zY",
+        authDomain: "native-collegebeats.firebaseapp.com",
+        databaseURL: "https://native-collegebeats-default-rtdb.firebaseio.com",
+        projectId: "native-collegebeats",
+        storageBucket: "native-collegebeats.appspot.com",
+        messagingSenderId: "870351467576",
+        appId: "1:870351467576:web:916f19c1925c0d31cf22cd",
+        measurementId: "G-QTKFHTVDHS"
+      };
+      // Initialize Firebase
+    
+
+
+
+ 
+    
